175655b8ac46813086e32e795378be18aba378d3
Code Sandbox <hello@codesandbox.io>
2021-07-05 12:08:09 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.6ba37860.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{4:function(e,t){window.addEventListener("load",(function(){document.querySelector("#canvas").getContext("2d")}))},5:function(e,t,n){"use strict";n.r(t);var c=n(0),o=n(3),r=n.n(o),a=n(4),d=n.n(a),s=n(2),i=document.getElementById("root");r.a.render(Object(s.jsx)(c.StrictMode,{children:Object(s.jsx)(d.a,{})}),i)}},[[5,1,2]]]);
+//# sourceMappingURL=main.6ba37860.chunk.js.map
